ea3eb631cbb4276763670fba0d561e0fbc20aa7f
Xondee <95598346+Xondee@users.noreply.github.com>
2023-11-01 09:09:19 -0500

    Create action_page.php

---
+++ action_page.php
@@ -0,0 +1,22 @@
+<html>
+<head>
+  <style>
+	html, body {
+    pointer-events: none;
+		margin:0;
+		padding:0;
+}
+	iframe {
+		
+		position: fixed;
+		height: 100%;
+		width: 100%;
+		border:none;
+}
+  </style>
+</head>
+<body data-new-gr-c-s-check-loaded="14.1135.0" data-gr-ext-installed=""><div class="outer">
+  <iframe width="1920" height="1080" src=link frameborder="0" allowfullscreen=""></iframe>
+</div>
+</body>
+</html>
